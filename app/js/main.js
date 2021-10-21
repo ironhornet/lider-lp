40b3d731639b335782ffcc3fd531ff9bdb1188d9
ironhornet <tarmyrka@gmail.com>
2021-10-21 22:22:32 +0300

    finished project , added adaptive layout

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$("body").on("click", ".footer-link", function () { var e = $($(this).attr("href")).offset().top; return $("html,body").animate({ scrollTop: e }, 800), !1 });
+
+$("body").on("click", ".events-link", function () { var e = $($(this).attr("href")).offset().top; return $("html,body").animate({ scrollTop: e }, 800), !1 });
+
+$("body").on("click", ".form-link", function () { var e = $($(this).attr("href")).offset().top; return $("html,body").animate({ scrollTop: e }, 800), !1 });
+
+$("body").on("click", ".about-link", function () { var e = $($(this).attr("href")).offset().top; return $("html,body").animate({ scrollTop: e }, 800), !1 });
+
+$("body").on("click", ".programs-link", function () { var e = $($(this).attr("href")).offset().top; return $("html,body").animate({ scrollTop: e }, 800), !1 });
